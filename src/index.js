0a3016465a32b98632cdd142bdf4214b7c388c68
skr18 <sujeetrath123@gmail.com>
2024-03-27 13:56:12 +0530

    connect to mongoDb

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+//import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
