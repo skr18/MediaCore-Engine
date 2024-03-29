3a5c820b7680b8338ec985cbb5d75fb4d2b96524
skr18 <sujeetrath123@gmail.com>
2024-03-29 14:05:38 +0530

    added user routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,9 @@ app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//router import
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
+
 export {app}
