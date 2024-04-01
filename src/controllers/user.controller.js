d401d6d6f6cd7beb3277154be6178645f8d4564d
skr18 <sujeetrath123@gmail.com>
2024-04-01 21:34:31 +0530

    add change user password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -165,4 +165,16 @@ const refreshAccessToekn = asyncHandler(async(req,res)=>{
 
 })
 
+const changePassword = asyncHandler(async (req,res)=>{
+    const {oldPassword, newPassword} = req.body
+    const user = await User.findById(req.user._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new apiError(400,"invalid old password")
+    }
+    user.password = newPassword
+    user.save({validateBeforeSave:false})
+    return res.status(200)
+            .json(new apiResponse(200,{},"password updated successfully"))
+})
 export {registerUser , loginUser, logoutUser, refreshAccessToekn}
